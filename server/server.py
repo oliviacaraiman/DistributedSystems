a897c9f1f5395ef11bc3a8521000c103872da554
oliviacaraiman <caraimanolivia@gmail.com>
2019-11-10 17:00:09 +0100

    first commit

---
+++ server/server.py
@@ -0,0 +1,181 @@
+# coding=utf-8
+# ------------------------------------------------------------------------------------------------------
+# TDA596 - Lab 1
+# server/server.py
+# Input: Node_ID total_number_of_ID
+# Student: John Doe
+# ------------------------------------------------------------------------------------------------------
+import traceback
+import sys
+import time
+import json
+import argparse
+from threading import Thread
+
+from bottle import Bottle, run, request, template
+import requests
+# ------------------------------------------------------------------------------------------------------
+try:
+    app = Bottle()
+
+    #board = "nothing" 
+    board = {}
+
+
+    # ------------------------------------------------------------------------------------------------------
+    # BOARD FUNCTIONS
+    # You will probably need to modify them
+    # ------------------------------------------------------------------------------------------------------
+    def add_new_element_to_store(entry_sequence, element, is_propagated_call=False):
+        print "I am in the add_new_element_to_store function"
+        global board, node_id
+        success = False
+        try:
+            for e in board:
+                assert e != element                    
+            board[entry_sequence] = element
+            #board = element
+            success = True
+        except AssertionError:
+            print "Already exist element"
+        except Exception as e:
+            print e
+        return success
+
+    def modify_element_in_store(entry_sequence, modified_element, is_propagated_call = False):
+        global board, node_id
+        success = False
+        try:
+            board = modified_element
+            success = True
+        except Exception as e:
+            print e
+        return success
+
+    def delete_element_from_store(entry_sequence, is_propagated_call = False):
+        global board, node_id
+        success = False
+        try:
+            board = ""
+            success = True
+        except Exception as e:
+            print e
+        return success
+
+    # ------------------------------------------------------------------------------------------------------
+    # DISTRIBUTED COMMUNICATIONS FUNCTIONS
+    # ------------------------------------------------------------------------------------------------------
+    def contact_vessel(vessel_ip, path, payload = None, req='POST'):
+        # Try to contact another server (vessel) through a POST or GET, once
+        print "I am in contact_vessel function"
+        success = False
+        try:
+            if 'POST' in req:
+                res = requests.post('http://{}{}'.format(vessel_ip, path), data=payload)
+            elif 'GET' in req:
+                res = requests.get('http://{}{}'.format(vessel_ip, path))
+            else:
+                print 'Non implemented feature!'
+            # result is in res.text or res.json()
+            print(res.text)
+            if res.status_code == 200:
+                success = True
+        except Exception as e:
+            print e
+        return success
+
+    def propagate_to_vessels(path, payload = None, req = 'POST'):
+        print "I am in propagate_to_vessels function"
+        global vessel_list, node_id
+
+        for vessel_id, vessel_ip in vessel_list.items():
+            if int(vessel_id) != node_id: # don't propagate to yourself
+                success = contact_vessel(vessel_ip, path, payload, req)
+                if not success:
+                    print "\n\nCould not contact vessel {}\n\n".format(vessel_id)
+
+
+    # ------------------------------------------------------------------------------------------------------
+    # ROUTES
+    # ------------------------------------------------------------------------------------------------------
+    # a single example (index) for get, and one for post
+    # ------------------------------------------------------------------------------------------------------
+    @app.route('/')
+    def index():
+        global board, node_id
+        return template('server/index.tpl', board_title='Vessel {}'.format(node_id), board_dict=sorted(board.iteritems()), members_name_string='Lucas BERNEL')
+
+    @app.get('/board')
+    def get_board():
+        global board, node_id
+        print board
+        return template('server/boardcontents_template.tpl',board_title='Vessel {}'.format(node_id), board_dict=sorted(board.iteritems()))
+    # ------------------------------------------------------------------------------------------------------
+    @app.post('/board')
+    def client_add_received():
+        print "I am in client_add_received"
+        '''Adds a new element to the board
+        Called directly when a user is doing a POST request on /board'''
+        global board, node_id
+        try:
+            new_entry = request.forms.get('entry')
+            entry_seq = len(board) + 1
+            add_new_element_to_store(entry_seq, new_entry) # you might want to change None here
+            # you should propagate something
+            # Please use threads to avoid blocking
+            # thread = Thread(target=???,args=???)
+            thread = Thread(target=propagate_to_vessels, args=('/propagate/add/'+str(entry_seq), board))
+            thread.start()
+            # you should create the thread as a deamon with thread.daemon = True
+            # then call thread.start() to spawn the thread
+            #Propagate to other vessels
+            #propagate_to_vessels('/propagate/add/'+str(entry_seq), payload=board, req = 'POST')
+            return True
+        except Exception as e:
+            print e
+        return False
+
+    @app.post('/board/<element_id:int>/')
+    def client_action_received(element_id):
+        print "I am in client_action_received function"
+        # todo
+        pass
+
+    @app.post('/propagate/<action>/<element_id>')
+    def propagation_received(action, element_id):
+        print "I am in propagation_received function"
+        postdata=request.body.read()
+        print postdata
+        element = postdata.split("=")
+        add_new_element_to_store(element_id, element[0])
+        # todo
+        pass
+        
+    # ------------------------------------------------------------------------------------------------------
+    # EXECUTION
+    # ------------------------------------------------------------------------------------------------------
+    def main():
+        global vessel_list, node_id, app
+
+        port = 80
+        parser = argparse.ArgumentParser(description='Your own implementation of the distributed blackboard')
+        parser.add_argument('--id', nargs='?', dest='nid', default=1, type=int, help='This server ID')
+        parser.add_argument('--vessels', nargs='?', dest='nbv', default=1, type=int, help='The total number of vessels present in the system')
+        args = parser.parse_args()
+        node_id = args.nid
+        vessel_list = dict()
+        # We need to write the other vessels IP, based on the knowledge of their number
+        for i in range(1, args.nbv):
+            vessel_list[str(i)] = '10.1.0.{}'.format(str(i))
+
+        try:
+            run(app, host=vessel_list[str(node_id)], port=port)
+        except Exception as e:
+            print e
+    # ------------------------------------------------------------------------------------------------------
+    if __name__ == '__main__':
+        main()
+except Exception as e:
+        traceback.print_exc()
+        while True:
+            time.sleep(60.)
